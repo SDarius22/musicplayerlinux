27fa60fb4a485bb494ada3dd0902c7410dfb9f7b
SDarius22 <saladarius72@gmail.com>
2024-08-20 20:31:21 +0300

    added win config

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,32 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <audiotags/audiotags_plugin_c_api.h>
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <desktop_drop/desktop_drop_plugin.h>
+#include <file_saver/file_saver_plugin.h>
+#include <objectbox_flutter_libs/objectbox_flutter_libs_plugin.h>
+#include <system_tray/system_tray_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  AudiotagsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudiotagsPluginCApi"));
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  DesktopDropPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("DesktopDropPlugin"));
+  FileSaverPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSaverPlugin"));
+  ObjectboxFlutterLibsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ObjectboxFlutterLibsPlugin"));
+  SystemTrayPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SystemTrayPlugin"));
+}
